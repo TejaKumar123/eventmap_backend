8e2d0185ce0899ff280edbab9e05dc9d1756d2aa
TejaKumar123 <inceptioncom414@gmail.com>
2024-12-13 01:10:24 +0530

    small changes

--- src/config/redisConfig.js
+++ src/config/redisConfig.js
@@ -1,9 +1,18 @@
 import { createClient } from "redis";
 
-const redisClient = createClient({
+/* const redisClient = createClient({
 	host: "localhost",
 	port: "6379"
-})
+}) */
+
+const redisClient = createClient({
+	username: 'default',
+	password: `${process.env.REDIS_PASSWORD}`,
+	socket: {
+		host: `${process.env.REDIS_HOST}`,
+		port: `${process.env.REDIS_PORT}`
+	}
+});
 
 const redisConnect = async () => {
 	redisClient.connect()
